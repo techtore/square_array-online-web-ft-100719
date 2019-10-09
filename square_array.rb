52761163e308f2c13c64fca534810e7d29a6c5db
Victoria Hale <vic.hale74@gmail.com>
2019-10-09 18:28:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  new_array= []
-  array = array.each{|num| num**2}
-  new_array << array
-
-
+  new_array = []
+  array.each do |num| 
+  new_array << num ** 2
+  end 
+  return new_array
 end
   
  
